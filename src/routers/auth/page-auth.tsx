141a9773d84133f5ab5f5fdb8c6c1c0de1ce7f4b
Redreev8 <redreevs@mail.ru>
2025-05-24 18:06:04 +0500

    add page auth

---
+++ src/routers/auth/page-auth.tsx
@@ -0,0 +1,25 @@
+import type { FC } from 'react'
+import Container from '../../ui/container'
+import { VisuallyHidden } from 'radix-ui'
+import Title from '../../ui/title'
+import Card from '../../ui/card'
+import FormAuth from './components/form-auth'
+
+const PageAuth: FC = () => {
+    return (
+        <main>
+            <section>
+                <Container className="flex min-h-screen flex-col items-center justify-center">
+                    <VisuallyHidden.Root asChild>
+                        <Title as={1}>Authorization</Title>
+                    </VisuallyHidden.Root>
+                    <Card className="w-full max-w-[720px]">
+                        <FormAuth />
+                    </Card>
+                </Container>
+            </section>
+        </main>
+    )
+}
+
+export default PageAuth
