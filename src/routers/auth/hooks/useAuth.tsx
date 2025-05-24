293d50fcd8100bbdce3910ae008bc39b9e45fca6
Redreev8 <redreevs@mail.ru>
2025-05-24 18:04:37 +0500

    add hook useAuth

---
+++ src/routers/auth/hooks/useAuth.tsx
@@ -0,0 +1,40 @@
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { formSchemaAuth, type FormFieldsAuth } from '../type/auth'
+import apiAuth from '../api/auth'
+import type { AxiosError } from 'axios'
+import { useNavigate } from 'react-router'
+
+const useAuth = () => {
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors },
+    } = useForm<FormFieldsAuth>({
+        mode: 'onBlur',
+        reValidateMode: 'onBlur',
+        resolver: zodResolver(formSchemaAuth),
+    })
+    const navigate = useNavigate()
+    const auth = async (d: FormFieldsAuth) => {
+        try {
+            const { data } = (await apiAuth(d)) as { data: string }
+            localStorage.setItem('auth-token', data)
+            navigate('/links')
+        } catch (e) {
+            const { response } = e as AxiosError
+            const data = response!.data as string
+            setError('name', { message: data })
+            setError('password', { message: data })
+        }
+    }
+
+    return {
+        register,
+        auth: handleSubmit(auth),
+        errors,
+    }
+}
+
+export default useAuth
