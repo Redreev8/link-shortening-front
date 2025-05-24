3c03d6c3b23c356a665ecaa1e26ea5aa0e1f9a94
Redreev8 <redreevs@mail.ru>
2025-05-24 18:06:25 +0500

    add page auth

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,13 +1,20 @@
 import type { FC } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import PageLinksList from './links/page-links-list'
+import PageAuth from './auth/page-auth'
+import LayoutAuth from './auth/layout-auth'
 
 const Routers: FC = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route>
-                    <Route path="/links" element={<PageLinksList />} />
+                <Route element={<LayoutAuth />}>
+                    <Route>
+                        <Route path="/links" element={<PageLinksList />} />
+                    </Route>
+                    <Route>
+                        <Route path="/auth" element={<PageAuth />} />
+                    </Route>
                 </Route>
             </Routes>
         </BrowserRouter>
