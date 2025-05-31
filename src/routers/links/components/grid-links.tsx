d2ecfd1dc6b62c668f2c4c1dca094a095315ca20
Redreev8 <redreevs@mail.ru>
2025-06-01 01:05:33 +0500

    change grid responce links

--- src/routers/links/components/grid-links.tsx
+++ src/routers/links/components/grid-links.tsx
@@ -2,14 +2,20 @@ import type { FC } from 'react'
 import type { Link } from '../type/link'
 import CardLink from './card-link'
 import CardLinkCreate from './card-link-create'
+import classNames from 'classnames'
 
 interface GridLinksProps {
     links: Link[]
 }
 
 const GridLinks: FC<GridLinksProps> = ({ links }) => {
+    const cl = classNames(
+        'grid gap-1',
+        '2xl:grid-cols-[repeat(4,1fr)] lg:grid-cols-[repeat(3,1fr)]',
+        'grid-cols-[repeat(2,1fr)] max-sm:grid-cols-[repeat(1,1fr)]',
+    )
     return (
-        <ul className="grid grid-cols-[repeat(auto-fit,minmax(360px,1fr))] gap-1">
+        <ul className={cl}>
             <li>
                 <CardLinkCreate />
             </li>
