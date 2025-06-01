470ad96752f9b6384b6bbfbee52dd70bf6831e31
Redreev8 <redreevs@mail.ru>
2025-06-01 13:45:59 +0500

    fix grid links

--- src/routers/links/components/grid-links.tsx
+++ src/routers/links/components/grid-links.tsx
@@ -11,8 +11,8 @@ interface GridLinksProps {
 const GridLinks: FC<GridLinksProps> = ({ links }) => {
     const cl = classNames(
         'grid gap-1',
-        '2xl:grid-cols-[repeat(4,1fr)] lg:grid-cols-[repeat(3,1fr)]',
-        'grid-cols-[repeat(2,1fr)] max-sm:grid-cols-[repeat(1,1fr)]',
+        '2xl:grid-cols-[repeat(4,minmax(360px,1fr))] lg:grid-cols-[repeat(3,minmax(320px,1fr)))]',
+        'grid-cols-[repeat(2,minmax(320px,1fr)))] max-sm:grid-cols-[repeat(1,100%)]',
     )
     return (
         <ul className={cl}>
